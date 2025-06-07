efd007caeaf1aaaad70664d3ba84892ef691ecb9
dragomir1520 <dragomir1520@gmail.com>
2025-06-08 02:57:41 +0300

    build: init migrate prod db

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250607_235517 from './20250607_235517';
+
+export const migrations = [
+  {
+    up: migration_20250607_235517.up,
+    down: migration_20250607_235517.down,
+    name: '20250607_235517',
+  },
+];
