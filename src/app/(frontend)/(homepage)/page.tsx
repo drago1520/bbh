25f2f2340ee9a5340c9ae682422e1e9feb6cc266
dragomir1520 <dragomir1520@gmail.com>
2025-06-15 14:07:54 +0300

    fix: blocks are returned as 'undefined' but Typescript expected an array chore: removed dynamic payload routing completely. Too many problems

--- src/app/(frontend)/(homepage)/page.tsx
+++ src/app/(frontend)/(homepage)/page.tsx
@@ -33,7 +33,7 @@ export default async function HomePage() {
         <NetworkingEvents />
         <PartnersCarousel />
         <Conference isImageRight={false} />
-        <FAQsThree homepageBlocks={homePage.blocks} />
+        <FAQsThree homepageBlocks={homePage?.blocks} />
         <News />
       </main>
       <Footer />
