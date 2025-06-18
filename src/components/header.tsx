bc14965335ab59d3e3fce92cdfc386ca26c14a5a
dragomir1520 <dragomir1520@gmail.com>
2025-06-18 19:13:27 +0300

    fix/ui: опростяване на header #27 chore: changed mobile menu description text

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -7,64 +7,33 @@ import Logo from '@/components/Icons';
 import { NavigationMenu, NavigationMenuContent, NavigationMenuItem, NavigationMenuLink, NavigationMenuList, NavigationMenuTrigger } from '@/components/ui/navigation-menu';
 import NoWebpageYetAlertDialogWrapper from '../app/(frontend)/coming-soon/components/no-webpage-yet-modal';
 import MobileNavMenu from './mobile-nav-menu';
+import { Button } from './ui/button';
 
 export type NavProps = { title: string; href: string; description: string; className?: string };
 const navigationItems: NavProps[] = [
   {
     title: 'БИЗНЕС ЗАКУСКА',
     href: '#',
-    description: 'Закуска с малка група предприемачи от Бургас',
+    description: 'Представете бизнеса си на друго ниво пред избрана аудитория',
     className: 'border-brand-accent',
   },
   {
     title: 'НЕТУЪРКИНГ',
     href: '#',
-    description: 'Разшири контактите си. Спечели нови партньори, клиенти и служители',
+    description: 'Мястото, където ще срещнете предприемачи като вас',
     className: 'border-brand-accent',
   },
   {
     title: 'ОБУЧЕНИЯ',
     href: '#',
-    description: 'Скалирай бизнеса си и придобии десетилетия опит за 3 месеца',
+    description: 'Прекият път към успешния бизнес',
     className: 'border-brand-purple',
   },
   {
     title: 'КОНФЕРЕНЦИЙ',
     href: '#',
-    description: 'Участвай в уъркшоп за скалиране и чуй какво имат да кажат лекторите за резултата ти',
+    description: 'Актуални знания от предприемачи за предприемачи',
     className: 'border-brand-orange',
-  },
-];
-const networkingTabs: NavProps[] = [
-  {
-    title: 'Виж Страницата',
-    href: '/networking',
-    description: 'Отиди на страницата за нетуъркинг. Всичко свързано за нетуъркинга.',
-  },
-  {
-    title: 'Предстоящи събития',
-    href: '#',
-    description: '<TODO Заглавие на събитието>',
-  },
-  {
-    title: 'Запиши се',
-    href: '#',
-    description: 'Ще ти изпраим имейл с покана за Google и Apple calendar.',
-  },
-  {
-    title: 'Минали събития',
-    href: '#',
-    description: '<TODO заглавие на миналото събитие>',
-  },
-  {
-    title: 'Как ние нетуъркваме?',
-    href: '#',
-    description: 'Интересувай се от хората. Кого изобщо искаш да срещнеш? Давай стойност, която би струвала пари.',
-  },
-  {
-    title: 'Не ти стига веднъж в месеца?',
-    href: '/networking',
-    description: 'Пиши си, когато ти е удобно с хора, 2 нива над теб в нашия Discord.',
   },
 ];
 
@@ -76,85 +45,13 @@ export default function Header() {
           <Link href="/" className="group flex items-center">
             <Logo />
           </Link>
-
           {/* Desktop Navigation */}
-          <nav className="hidden items-center space-x-6 xl:flex xl:space-x-8" aria-label="Main navigation">
-            <NavigationMenu>
-              <NavigationMenuList>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>НЕТУЪРКИНГ</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px]">
-                      {networkingTabs.map(component => (
-                        <ListItem key={component.title} title={component.title} href={component.href}>
-                          {component.description}
-                        </ListItem>
-                      ))}
-                    </ul>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-                {/* <NavigationMenuItem>
-                  <NavigationMenuTrigger>БИЗНЕС ЗАКУСКА</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                      <li className="row-span-3">
-                        <NavigationMenuLink asChild>
-                          <Link className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none select-none focus:shadow-md" href="#">
-                            <Logo className='size-full' />
-                            <div className="mb-2 text-lg font-medium">Главна страница</div>
-                            <p className="text-muted-foreground text-sm leading-tight">Открий нови клиенти и партньори на следващата ни бизнес закуска.</p>
-                          </Link>
-                        </NavigationMenuLink>
-                      </li>
-                      <ListItem href="#" title="Запиши се">
-                        TODO детайли за следващата закуска
-                      </ListItem>
-                      <ListItem href="#" title="Какво е Бизнес Закуската?">
-                        How to install dependencies and structure your app.
-                      </ListItem>
-                      <ListItem href="#" title="Минали събития">
-                        Разгледай участници, мнения и какво се е случило.
-                      </ListItem>
-                    </ul>
-                  </NavigationMenuContent>
-                </NavigationMenuItem> */}
-                <NavigationMenuItem>
-                  <NavigationMenuLink asChild>
-                    <Link href="#">БИЗНЕС ЗАКУСКА</Link>
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuTrigger>КОНФЕРЕНЦИИ</NavigationMenuTrigger>
-                  <NavigationMenuContent>
-                    <ul className="grid gap-3 p-6 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
-                      <li className="row-span-3">
-                        <NavigationMenuLink asChild className="from-muted/50 to-muted flex h-full w-full flex-col justify-end rounded-md bg-gradient-to-b p-6 no-underline outline-none select-none focus:shadow-md">
-                          <Link href="/">
-                            <Logo className="h-6 w-6" />
-                            <div className="mt-4 mb-2 text-lg font-medium">shadcn/ui</div>
-                            <p className="text-muted-foreground text-sm leading-tight">Beautifully designed components that you can copy and paste into your apps. Accessible. Customizable. Open Source.</p>
-                          </Link>
-                        </NavigationMenuLink>
-                      </li>
-                      <ListItem href="#" title="Introduction">
-                        Re-usable components built using Radix UI and Tailwind CSS.
-                      </ListItem>
-                      <ListItem href="#" title="Installation">
-                        How to install dependencies and structure your app.
-                      </ListItem>
-                      <ListItem href="#" title="Typography">
-                        Styles for headings, paragraphs, lists...etc
-                      </ListItem>
-                    </ul>
-                  </NavigationMenuContent>
-                </NavigationMenuItem>
-                <NavigationMenuItem>
-                  <NavigationMenuLink asChild>
-                    <Link href="#">ОБУЧЕНИЯ</Link>
-                  </NavigationMenuLink>
-                </NavigationMenuItem>
-              </NavigationMenuList>
-            </NavigationMenu>
+          <nav className="hidden items-center xl:flex" aria-label="Main navigation">
+            {navigationItems.map(({ title, href }) => (
+              <Button variant="link" key={title}>
+                <Link href={href}>{title}</Link>
+              </Button>
+            ))}
             {process.env.NODE_ENV === 'development' && <ThemeSelector />}
           </nav>
 
