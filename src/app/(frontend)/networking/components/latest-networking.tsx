df3fe4378ad08876c8a9269def7e2ee89f79814d
dragomir1520 <dragomir1520@gmail.com>
2025-06-29 12:35:57 +0300

    ui/latest-networking

--- src/app/(frontend)/networking/components/latest-networking.tsx
+++ src/app/(frontend)/networking/components/latest-networking.tsx
@@ -2,7 +2,7 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { Badge } from '@/components/ui/badge';
-import { ArrowRight, Calendar, MapPin, Plus } from 'lucide-react';
+import { ArrowRight, Calendar, ExternalLink, MapPin, Plus } from 'lucide-react';
 import { ComponentProps } from 'react';
 import { cn } from '@/lib/utils';
 import Link from 'next/link';
@@ -13,9 +13,7 @@ import { Event } from '@/payload-types';
 import { formatToBulgarianDate } from '@/utils/format-bulgarian-datetime';
 import RichText from '@/components/RichText';
 import { MediaSection } from '@/components/Sections/content-with-media-and-button';
-import Statistics from '@/components/Sections/statistics';
 import CountdownTimer from './countdown';
-import CountUp from 'react-countup';
 
 /**
  * @description Image is the right (dekstop) & up (mobile) by default.
@@ -28,30 +26,29 @@ export default function LatestNetworking({ isImageRight = false, event, classNam
   const showLocation = location && (locationImg || location.toLowerCase().includes('gravity'));
   return (
     <section aria-labelledby="networking-heading" className={cn('pb-32', className)} {...props}>
-      <div className="container py-32">
-        <div className="grid justify-center gap-6 sm:gap-8 xl:grid-cols-4 xl:gap-12">
-          <MediaSection isImageRight={isImageRight} className="xl:bg-muted/40 space-y-16 rounded-md xl:col-span-4 xl:p-4">
-            <div className="relative aspect-[1920/1005]">
-              <Image src={thumbnail.url || ''} alt={thumbnail.alt} width={thumbnail?.width || 1920} height={thumbnail.height || 1005} className="rounded-md object-contain shadow-xl" />
-            </div>
-            {/* <div className='hidden xl:block'>
-              <div className="grid grid-cols-2 justify-center gap-8 text-center text-2xl sm:text-xl">
-                <div className="dark:bg-accent bg-background z-10 flex flex-col items-center justify-center gap-4 rounded p-4 shadow-md dark:border">
-                  <Image src="/networking.svg" alt="networking icon" width={48} height={48} />
-                  <dd className="mt-2 font-bold">
-                    Кога?
-                  </dd>
-                  <dt className="text-muted-foreground text-base">17 юли, 19:00 часа</dt>
-                </div>
-                <div className="dark:bg-accent bg-background z-10 flex flex-col items-center justify-center gap-4 rounded p-4 shadow-md dark:border">
-                  <Image src="/networking.svg" alt="networking icon" width={48} height={48} />
-                  <dd className="mt-2 font-bold">
-                    Къде?
-                  </dd>
-                  <dt className="text-muted-foreground text-base">Gravity Ruin Bar, ет.2, Бургас</dt>
+      <div className="container space-y-8 py-32 sm:space-y-16 xl:space-y-20">
+        <div className="relative aspect-[1920/1005]">
+          <Image src={thumbnail.url || ''} alt={thumbnail.alt} width={thumbnail?.width || 1920} height={thumbnail.height || 1005} className="rounded-md object-contain shadow-xl" />
+        </div>
+        <div className="grid justify-center gap-6 sm:gap-8 xl:grid-cols-2 xl:gap-12">
+          <MediaSection isImageRight={isImageRight} className="hidden space-y-16 rounded-md xl:block xl:bg-teal-50 xl:p-8">
+            {showLocation && (
+              <div className="grid h-60 grid-cols-2 justify-between gap-16">
+                <Link target="_blank" href={locationUrl || '#'} className="relative w-full rounded-md text-gray-50 shadow-md">
+                  <Image src={locationImg?.url || '/gravity-bar.jpg'} className="absolute top-0 left-0 h-full rounded-md object-cover" width={1920} height={160} alt={locationImg?.alt || 'Локация следващо бизнес събитие за нетуъркинг'} />
+                  <div className="relative z-10 flex h-full flex-col items-center justify-center gap-4">
+                    <h4 className="text-stroke-md text-xl font-semibold">Локация</h4>
+                    <address className="text-stroke-sm flex items-center gap-1 text-sm">
+                      <MapPin className="size-4" /> <span>{location}</span>
+                    </address>
+                  </div>
+                </Link>
+                {/* Unauthorized bug fix: #https://www.perplexity.ai/search/i-am-running-localhost-with-go-4._UjgRMQTKQ4SlfyA2pXg */}
+                <div className="overflow-hidden rounded-md shadow-md grayscale-75">
+                  <GoogleMapsEmbed apiKey={'AIzaSyBqcwYbGqE3Uc6xg7scMXeWoeKzItrlWmw'} mode="place" width="100%" height={384} zoom="14" q={location} />
                 </div>
               </div>
-            </div> */}
+            )}
             <CountdownTimer className="hidden xl:flex" endDate={date} />
           </MediaSection>
           <article className="xl:col-span-1">
@@ -59,56 +56,41 @@ export default function LatestNetworking({ isImageRight = false, event, classNam
               {title}
             </h3>
             <div className="flex flex-col justify-start gap-4 font-semibold sm:flex-row sm:items-center">
-              <Link href="#" className="prose dark:prose-invert flex items-center gap-1">
+              <Link href="#" className="prose dark:prose-invert decoration-muted-foreground flex items-center gap-1 underline underline-offset-4">
                 <Calendar className="size-4" /> {formatToBulgarianDate(date)}
               </Link>
-              <Link href="#" className="prose dark:prose-invert flex items-center gap-1">
-                <MapPin className="size-4" /> {location}
+              <Link href="#" className="prose dark:prose-invert decoration-muted-foreground flex items-center gap-1 underline underline-offset-4">
+                <MapPin className="size-4" /> <span>{location}</span>
               </Link>
             </div>
-            <div className="mt-8">
+            <div className="mt-6">
               <RichText data={description} />
             </div>
             <div className="mt-12 xl:hidden">
               <CountDownTimer endDate={date} />
             </div>
-            {maxGuests && (
-              <span className="text-destructive mt-2 block text-sm italic sm:mt-4">
-                Местата за събитието са ограничени до <span className="font-semibold">{maxGuests}</span>
-              </span>
-            )}
-            <div className="mt-12">
-              <SignUpDialog eventId={event.id}>
-                <Button>
-                  ЗАПИШИ СЕ <Plus />
+            <div className="mt-8">
+              {maxGuests && (
+                <span className="text-destructive block text-sm italic">
+                  Местата за събитието са ограничени до <span className="font-semibold">{maxGuests}</span>
+                </span>
+              )}
+              <div className="mt-3">
+                <SignUpDialog eventId={event.id}>
+                  <Button>
+                    ЗАПИШИ СЕ <Plus />
+                  </Button>
+                </SignUpDialog>
+                <Button className="ml-4" variant="secondary" asChild>
+                  <Link href="#">
+                    ПРОЧЕТИ ПОВЕЧЕ <ArrowRight />
+                  </Link>
                 </Button>
-              </SignUpDialog>
-              <Button className="ml-4" variant="secondary" asChild>
-                <Link href="#">
-                  ПРОЧЕТИ ПОВЕЧЕ <ArrowRight />
-                </Link>
-              </Button>
+              </div>
             </div>
           </article>
         </div>
       </div>
-      {showLocation && (
-        <div className="container grid gap-16 xl:grid-cols-2">
-          <Link target="_blank" href={locationUrl || '#'} className="text-background dark:text-foreground relative block h-96 w-full">
-            <Image src={locationImg?.url || '/gravity-bar.jpg'} className="absolute top-0 left-0 h-full rounded-md object-cover" width={1920} height={160} alt={locationImg?.alt || 'Локация следващо бизнес събитие за нетуъркинг'} />
-            <div className="relative z-10 flex h-full flex-col items-center justify-center gap-4">
-              <h4 className="text-stroke-md text-xl font-semibold">Локация</h4>
-              <address className="text-stroke-sm flex items-center gap-1 text-sm">
-                <MapPin className="size-4" /> {location}
-              </address>
-            </div>
-          </Link>
-          {/* Unauthorized bug fix: #https://www.perplexity.ai/search/i-am-running-localhost-with-go-4._UjgRMQTKQ4SlfyA2pXg */}
-          <div className="overflow-hidden rounded-md grayscale-75">
-            <GoogleMapsEmbed apiKey={'AIzaSyBqcwYbGqE3Uc6xg7scMXeWoeKzItrlWmw'} mode="place" width="100%" height={384} zoom="14" q={location} />
-          </div>
-        </div>
-      )}
     </section>
   );
 }
