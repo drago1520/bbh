97c9848ea5c4be862b3d4cb741cae7c7ec888f09
dragomir1520 <dragomir1520@gmail.com>
2025-07-27 22:10:15 +0300

    fix/ts-error: stupid typescript error

--- src/app/(frontend)/networking/actions/add-attendee.ts
+++ src/app/(frontend)/networking/actions/add-attendee.ts
@@ -6,14 +6,14 @@ import { signUpSchema } from '../types';
 import payloadConfig from '@payload-config';
 import { getPayload } from 'payload';
 
-export async function addAttendee({ email, name, eventId }: z.infer<typeof signUpSchema> & { eventId: string }): Promise<{ success: true } | ErrorLogger> {
+export async function addAttendee({ email, name }: z.infer<typeof signUpSchema> & { eventId: string }): Promise<{ success: true } | ErrorLogger> {
   try {
     const config = await payloadConfig;
     const payload = await getPayload({ config: config });
 
     const newAttendee = await payload.create({
       collection: 'attendees',
-      data: { email, name, event: eventId },
+      data: { email, name },
     });
     if (!newAttendee) throw new Error('Грешка: Няма записан ред в базата данни.');
     return { success: true };
