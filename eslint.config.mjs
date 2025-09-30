7af6180218d15a377ac43015634b3ced42272131
dragomir1520 <dragomir1520@gmail.com>
2025-09-30 13:19:56 +0300

    ci: eslint ignore auto gen files

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,36 +1,39 @@
-import { dirname } from "path"
-import { fileURLToPath } from "url"
-import { FlatCompat } from "@eslint/eslintrc"
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-	baseDirectory: __dirname,
-})
+  baseDirectory: __dirname,
+});
 
 const eslintConfig = [
-	...compat.extends("next/core-web-vitals", "next/typescript"),
-	{
-		rules: {
-			"@typescript-eslint/ban-ts-comment": "warn",
-			"@typescript-eslint/no-empty-object-type": "warn",
-			"@typescript-eslint/no-explicit-any": "warn",
-			"@typescript-eslint/no-unused-vars": [
-				"warn",
-				{
-					vars: "all",
-					args: "after-used",
-					ignoreRestSiblings: false,
-					argsIgnorePattern: "^_",
-					varsIgnorePattern: "^_",
-					destructuredArrayIgnorePattern: "^_",
-					caughtErrorsIgnorePattern: "^(_|ignore)",
-				},
-			],
-			"react/no-unescaped-entities": "off",
-		},
-	},
-]
+  {
+    ignores: ['**/migrations/**', 'src/payload/**'],
+  },
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/ban-ts-comment': 'warn',
+      '@typescript-eslint/no-empty-object-type': 'warn',
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          args: 'after-used',
+          ignoreRestSiblings: false,
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          destructuredArrayIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^(_|ignore)',
+        },
+      ],
+      'react/no-unescaped-entities': 'off',
+    },
+  },
+];
 
-export default eslintConfig
+export default eslintConfig;
