2601729cc655670b0acc06e059a049ed44a96618
drago1520 <141066422+drago1520@users.noreply.github.com>
2025-10-31 11:23:35 +0200

    fix/bugsink: wrong path fixed

--- src/intrumentation.ts
+++ src/intrumentation.ts
@@ -1,7 +1,7 @@
 import * as Sentry from '@sentry/nextjs'
 
 export async function register() {
-  await import('./lib/sentry.server.config')
+  await import('../sentry.server.config')
 }
 // Capture errors from nested React Server Components
 export const onRequestError = Sentry.captureRequestError
