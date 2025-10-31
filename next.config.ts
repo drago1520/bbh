162435d02784bdd95fe5c23d9242deec323525cd
drago1520 <141066422+drago1520@users.noreply.github.com>
2025-10-31 10:25:06 +0200

    feat: bugsink error tracking

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
-import { withPayload } from '@payloadcms/next/withPayload';
+import { withPayload } from '@payloadcms/next/withPayload'
+import { withSentryConfig } from '@sentry/nextjs'
 
-import redirects from './redirects.js';
-import { NextConfig } from 'next';
+import redirects from './redirects.js'
+import { NextConfig } from 'next'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000';
+const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : process.env.NEXT_PUBLIC_SITE_URL || 'http://localhost:3000'
 
 /** @type {import('next').NextConfig} */
 const nextConfig: NextConfig = {
@@ -24,19 +25,19 @@ const nextConfig: NextConfig = {
       '.cjs': ['.cts', '.cjs'],
       '.js': ['.ts', '.tsx', '.js', '.jsx'],
       '.mjs': ['.mts', '.mjs'],
-    };
+    }
 
-    return webpackConfig;
+    return webpackConfig
   },
   images: {
     remotePatterns: [
       ...[NEXT_PUBLIC_SERVER_URL].map(item => {
-        const url = new URL(item);
+        const url = new URL(item)
 
         return {
           hostname: url.hostname,
           protocol: url.protocol.replace(':', '') as 'http' | 'https',
-        };
+        }
       }),
       {
         protocol: 'https',
@@ -52,6 +53,6 @@ const nextConfig: NextConfig = {
   redirects,
   // This is required to support PostHog trailing slash API requests
   skipTrailingSlashRedirect: true,
-};
+}
 
-export default withPayload(nextConfig, { devBundleServerPackages: false });
+export default withPayload(nextConfig, { devBundleServerPackages: false })
