5bc17752cccf96aac1a62896edd9463ca8c23ab3
drago1520 <141066422+drago1520@users.noreply.github.com>
2025-11-23 14:52:16 +0200

    chore: add 3 fileds into row homepage collection

--- src/payload/pages/home-page-collection.ts
+++ src/payload/pages/home-page-collection.ts
@@ -27,45 +27,55 @@ export const HomePage: CollectionConfig = {
       ],
     },
     {
-      relationTo: "faqLeftRight",
-      name: "faqLeftRight",
-      type: "relationship",
-      label: "FAQs",
+      type: "row",
+      fields: [
+        {
+          relationTo: "faqLeftRight",
+          name: "faqLeftRight",
+          type: "relationship",
+          label: "FAQs",
+        },
+        {
+          relationTo: "gallery7",
+          name: "gallery7",
+          type: "relationship",
+          label: "Снимки минали събития",
+        },
+        {
+          relationTo: "testimonial25",
+          name: "testimonials25",
+          type: "relationship",
+          label: "Testimonials",
+        },
+      ],
     },
     {
-      relationTo: "gallery7",
-      name: "gallery7",
-      type: "relationship",
-      label: "Снимки минали събития",
-    },
-    {
-      relationTo: "testimonial25",
-      name: "testimonials25",
-      type: "relationship",
-      label: "Testimonials",
-    },
-    {
-      relationTo: "events",
-      name: "events",
-      type: "relationship",
-      hasMany: true,
-      label: "Събитие на показ",
-      admin: {
-        description:
-          "Ако не въведете, ще се показват всички включени актуални събития",
-      },
-    },
-    {
-      relationTo: "statisticsN",
-      name: "statisticsN",
-      type: "relationship",
-      label: "3 Статистики",
-    },
-    {
-      relationTo: "partnersN",
-      name: "partners",
-      type: "relationship",
-      label: "Партньори лента",
+      type: "row",
+      fields: [
+        {
+          relationTo: "events",
+          name: "events",
+          type: "relationship",
+          hasMany: true,
+          label: "Събитие на показ",
+          admin: {
+            description:
+              "Ако не въведете, ще се показват всички включени актуални събития",
+          },
+        },
+        {
+          relationTo: "statisticsN",
+          name: "statisticsN",
+          type: "relationship",
+          label: "3 Статистики",
+        },
+        {
+          relationTo: "partnersN",
+          name: "partners",
+          type: "relationship",
+          label: "Партньори лента",
+        },
+      ],
     },
     {
       type: "text",
